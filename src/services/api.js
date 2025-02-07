c7d1bae9b7f7403e76bea19b1ba62a8fd39ca690
ravirv17 <ravirv1717@gmail.com>
2025-02-08 02:15:11 +0530

    Replaced the baseURL with Render URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({
+  baseURL: "https://event-management-platform-1-dcmq.onrender.com/api",
+});
 
 export default API;
